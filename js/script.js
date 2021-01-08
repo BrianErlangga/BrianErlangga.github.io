896c5858f028e94d8e75d7adfd75664c49a62d99
BrianErlangga <brian3rlangga@gmail.com>
2021-01-07 20:41:53 -0500

    changed everything

---
+++ js/script.js
@@ -0,0 +1,13 @@
+function aboutClick() {
+  document.getElementById('about').scrollIntoView();
+}
+
+function projectsClick() {
+  document.getElementById('projects').scrollIntoView();
+}
+
+function experienceClick() {
+  document.getElementById('experience').scrollIntoView();
+}
+
+//about projects experience 
